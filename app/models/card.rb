3ea988192e8f9dede93740071982493b6f527f61
nobyu <sakai.akinobu@gmail.com>
2018-06-28 13:42:26 +0900

    雑に json を parse してテーブルに沈める

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,26 @@
 class Card < ApplicationRecord
+
+  def self.build_by_json(json)
+    card = Card.new
+
+    card.cid = json["id"]
+    card.dbf_id = json["dbfId"]
+    card.name = json["name"]
+    card.description = json["description"]
+    card.flavor = json["flavor"]
+    card.artist = json["artist"]
+    card.attack = json["attack"]
+    card.card_class = json["cardClass"]
+    card.collectible = json["collectible"]
+    card.cost = json["cost"]
+    card.elite = json["elite"]
+    card.faction = json["faction"]
+    card.health = json["health"]
+    card.rarity = json["rarity"]
+    card.set = json["set"]
+    card.card_type = json["type"]
+
+    card
+  end
+
 end
