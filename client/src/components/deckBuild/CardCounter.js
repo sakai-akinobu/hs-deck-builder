ab5864e161474228226fc40cf4f4533873b8d5ba
nobyu <sakai.akinobu@gmail.com>
2018-06-29 08:45:48 +0900

    カードを枚数を表示

---
+++ client/src/components/deckBuild/CardCounter.js
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react';
+
+type CardCounterProps = {
+  count: number,
+};
+
+export default function CardCounter({count}: CardCounterProps) {
+  return (
+    <div>{count}/30</div>
+  );
+}
