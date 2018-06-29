ba8b81d47794ad1654ac20f4d05985d000bb88d8
nobyu <sakai.akinobu@gmail.com>
2018-06-29 14:46:04 +0900

    エンコードのレイアウトを修正

--- client/src/components/deckBuild/Deck.js
+++ client/src/components/deckBuild/Deck.js
@@ -4,19 +4,22 @@ import React from 'react';
 import DeckCard from './DeckCard';
 import ManaCurve from './ManaCurve';
 import CardCounter from './CardCounter';
+import EncodeString from './EncodeString';
 import type {DeckCard as DeckCardType} from '../../reducers/deckBuild/types';
 
 type DeckProps = {
+  hero: string,
   deck: DeckCardType[],
   unpickCard: (DeckCardType) => any,
 };
 
-export default function Deck({deck, unpickCard}: DeckProps) {
+export default function Deck({hero, deck, unpickCard}: DeckProps) {
   const cardCount = deck.reduce((cnt, deckCard) => cnt + deckCard.count, 0);
   return (
     <div>
       <ManaCurve deck={deck} />
       <CardCounter count={cardCount} />
+      <EncodeString hero={hero} deck={deck} />
       {deck.map((deckCard, index) => (
         <DeckCard key={index} deckCard={deckCard} onClick={() => unpickCard(deckCard)} />
       ))}
--- client/src/components/deckBuild/EncodeString.js
+++ client/src/components/deckBuild/EncodeString.js
@@ -1,12 +1,20 @@
 // @flow
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {encode} from 'deckstrings';
+import FaClose from 'react-icons/lib/fa/close';
+import FaCopy from 'react-icons/lib/fa/copy';
 
 import type {DeckCard as DeckCardType} from '../../reducers/deckBuild/types';
+import styles from './styles/EncodeString.scss';
 
-type DeckProps = {
+type EncodeStringProps = {
   hero: string,
   deck: DeckCardType[],
+};
+
+type EncodeStringState = {
+  isOpen: boolean,
 };
 
 const HERO_TO_DBF_ID_MAP = {
@@ -21,14 +29,72 @@ const HERO_TO_DBF_ID_MAP = {
   WARRIOR: 7,
 };
 
-export default function EncodeString({hero, deck}: DeckProps) {
-  const encodeString: string = encode({
-    cards: deck.map((deckCard) => [deckCard.card.dbf_id, deckCard.count]),
-    heroes: [HERO_TO_DBF_ID_MAP[hero]],
-    format: 2,
-  });
+export default class EncodeString extends React.Component<EncodeStringProps, EncodeStringState> {
 
-  return (
-    <div>{encodeString}</div>
-  );
+  encodeStringRef: any;
+
+  constructor() {
+    super();
+
+    this.state = {
+      isOpen: false,
+    };
+
+    this.encodeStringRef = React.createRef();
+  }
+
+  handleClickOpen = () => {
+    this.setState({isOpen: true});
+  }
+
+  handleRequestClose = () => {
+    this.setState({isOpen: false});
+  }
+
+  handleCopyEncodeString = () => {
+    if (this.encodeStringRef.current) {
+      this.encodeStringRef.current.select();
+      document.execCommand('copy');
+      this.setState({isOpen: false});
+    }
+  }
+
+  render() {
+    const {hero, deck} = this.props;
+    const encodeString: string = encode({
+      cards: deck.map((deckCard) => [deckCard.card.dbf_id, deckCard.count]),
+      heroes: [HERO_TO_DBF_ID_MAP[hero]],
+      format: 2,
+    });
+
+    return (
+      <div>
+        {this.state.isOpen && (
+          ReactDOM.createPortal((
+            <div className={styles.backdrop}>
+              <div className={styles.modal}>
+                <div className={styles.content}>
+                  <div className={styles.header}>
+                    <h2 className={styles.title}>デッキコード</h2>
+                    <span className={styles.closeButton}>
+                      <FaClose onClick={this.handleRequestClose} />
+                    </span>
+                  </div>
+                  <div className={styles.body}>
+                    <textarea value={encodeString} readOnly={true} className={styles.encodeString} ref={this.encodeStringRef} />
+                  </div>
+                  <div className={styles.footer}>
+                    <div className={styles.content}>
+                      <input type="button" value="コピー" onClick={this.handleCopyEncodeString} className={styles.copyButton} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ), window.document.body)
+        )}
+        <FaCopy onClick={this.handleClickOpen} className={styles.copyIcon} />
+      </div>
+    );
+  }
 }
