efde7275ce4d1ac9f6c5b3e0546a1152259dce14
sakai-akinobu <sakai.akinobu@gmail.com>
2019-02-11 19:41:30 +0900

    Migrate reducers/deckBuild/types.js to TypeScript.

---
+++ client/src/reducers/deckBuild/types.ts
@@ -0,0 +1,45 @@
+interface InitAction { type: 'hs-deck-builder/deckBuild/INIT', payload: any };
+interface ChangeHeroAction { type: 'hs-deck-builder/deckBuild/CHANGE_HERO', payload: any };
+interface SyncQueryAction { type: 'hs-deck-builder/deckBuild/SYNC_QUERY', payload: any };
+interface SearchCardAction { type: 'hs-deck-builder/deckBuild/SEARCH_CARD', payload: any };
+interface ChangePageAction { type: 'hs-deck-builder/deckBuild/CHANGE_PAGE', payload: any };
+interface PickCardAction { type: 'hs-deck-builder/deckBuild/PICK_CARD', payload: any };
+interface UnpickCardAction { type: 'hs-deck-builder/deckBuild/UNPICK_CARD', payload: any };
+
+export type DeckBuildAction =
+  | InitAction
+  | ChangeHeroAction
+  | SyncQueryAction
+  | SearchCardAction
+  | ChangePageAction
+  | PickCardAction
+  | UnpickCardAction
+  ;
+
+export interface Card {
+  id: string,
+  dbfId: string,
+  name: string,
+  cost: string,
+  rarity: string,
+}
+
+export interface DeckCard {
+  card: Card,
+  count: number,
+}
+
+export interface DeckBuildState {
+  format: string,
+  hero: string,
+  query: string,
+  mana: number | null,
+  page: {
+    prev: number | null,
+    current: number,
+    next: number | null,
+    last: number,
+  },
+  cards: Card[],
+  deck: DeckCard[],
+}
