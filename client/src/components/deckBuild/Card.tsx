329561215adb4e2fbbf76b780152a7b23cfe19fe
sakai-akinobu <sakai.akinobu@gmail.com>
2019-02-11 19:17:25 +0900

    Migrate components/deckBuild/Card.js to TypeScript.

--- client/src/components/deckBuild/Card.tsx
+++ client/src/components/deckBuild/Card.tsx
@@ -1,13 +1,12 @@
-// @flow
-import React from 'react';
+import * as React from 'react';
 
-import type {Card as CardType} from '../../reducers/deckBuild/types';
+import {Card as CardType} from '../../reducers/deckBuild/types';
 import styles from './styles/Card.scss';
 
-type CardProps = {
+interface CardProps {
   card: CardType,
   onClick: () => any,
-};
+}
 
 export default function Card({card, onClick}: CardProps) {
   return (
