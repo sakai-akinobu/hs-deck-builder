61af44546559054342cbbcc2186ec4d3e9aa6211
sakai-akinobu <sakai.akinobu@gmail.com>
2019-02-17 08:54:30 +0900

    Fix ESLint options.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,24 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+    },
+    "project": "./tsconfig.json",
   },
   plugins: [
+    "react",
     "@typescript-eslint",
   ],
   extends: [
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
   ],
   rules: {
     "indent": "off",
     "@typescript-eslint/indent": ["error", 2],
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
   },
 };
