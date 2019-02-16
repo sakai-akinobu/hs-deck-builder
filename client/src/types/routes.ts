b8b8cc1f822b555b551f21c8d09d8271e513eaaf
sakai-akinobu <sakai.akinobu@gmail.com>
2019-02-17 06:33:45 +0900

    Fix type annotations.

--- client/src/types/routes.ts
+++ client/src/types/routes.ts
@@ -1,16 +1,10 @@
 import {Store} from './index';
 
-export type HistoryProps = {
-  action: '' | 'POP' | 'PUSH',
-};
-
 export type LoaderProps = {
   store: Store,
-  url: string,
-  history: History & HistoryProps,
-};
+}
 
 export type Route = {
-  loader: (loaderProps: LoaderProps) => Promise<any> | Array<Promise<any>>,
+  loader: (loaderProps: LoaderProps) => Promise<any>,
   component: React.Component,
-};
+}
