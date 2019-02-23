35297b28f79d8fb5953041c5ca5d711bf7b8b013
sakai-akinobu <sakai.akinobu@gmail.com>
2019-02-23 11:19:04 +0900

    Use react hooks.

--- client/src/components/deckBuild/EncodeString.tsx
+++ client/src/components/deckBuild/EncodeString.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState, useRef} from 'react';
 import * as ReactDOM from 'react-dom';
 import {encode} from 'deckstrings';
 import {FaClose, FaCopy} from 'react-icons/lib/fa';
@@ -12,76 +13,52 @@ interface EncodeStringProps {
   deck: DeckCardType[];
 }
 
-interface EncodeStringState {
-  isOpen: boolean;
-}
+export default function EncodeString(props: EncodeStringProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const encodeStringRef = useRef<HTMLTextAreaElement>(null);
 
-export default class EncodeString extends React.Component<EncodeStringProps, EncodeStringState> {
+  const {hero, deck} = props;
+  const encodeString: string = encode({
+    cards: deck.map<[number, number]>((deckCard) => [Number(deckCard.card.dbfId), deckCard.count]),
+    heroes: [HeroToDbfIdMap[hero]],
+    format: 2,
+  });
 
-  encodeStringRef: any;
-
-  constructor(props: EncodeStringProps) {
-    super(props);
-
-    this.state = {
-      isOpen: false,
-    };
-
-    this.encodeStringRef = React.createRef();
-  }
-
-  handleClickOpen = () => {
-    this.setState({isOpen: true});
-  }
-
-  handleRequestClose = () => {
-    this.setState({isOpen: false});
-  }
-
-  handleCopyEncodeString = () => {
-    if (this.encodeStringRef.current) {
-      this.encodeStringRef.current.select();
+  const handleCopyEncodeString = () => {
+    if (encodeStringRef.current !== null) {
+      encodeStringRef.current.select();
       document.execCommand('copy');
-      this.setState({isOpen: false});
+      setIsOpen(false);
     }
-  }
+  };
 
-  render() {
-    const {hero, deck} = this.props;
-    const encodeString: string = encode({
-      cards: deck.map<[number, number]>((deckCard) => [Number(deckCard.card.dbfId), deckCard.count]),
-      heroes: [HeroToDbfIdMap[hero]],
-      format: 2,
-    });
-
-    return (
-      <div>
-        {this.state.isOpen && (
-          ReactDOM.createPortal((
-            <div className={styles.backdrop}>
-              <div className={styles.modal}>
-                <div className={styles.content}>
-                  <div className={styles.header}>
-                    <h2 className={styles.title}>デッキコード</h2>
-                    <span className={styles.closeButton}>
-                      <FaClose onClick={this.handleRequestClose} />
-                    </span>
-                  </div>
-                  <div className={styles.body}>
-                    <textarea value={encodeString} readOnly={true} className={styles.encodeString} ref={this.encodeStringRef} />
-                  </div>
-                  <div className={styles.footer}>
-                    <div className={styles.content}>
-                      <input type="button" value="コピー" onClick={this.handleCopyEncodeString} className={styles.copyButton} />
-                    </div>
+  return (
+    <div>
+      {isOpen && (
+        ReactDOM.createPortal((
+          <div className={styles.backdrop}>
+            <div className={styles.modal}>
+              <div className={styles.content}>
+                <div className={styles.header}>
+                  <h2 className={styles.title}>デッキコード</h2>
+                  <span className={styles.closeButton}>
+                    <FaClose onClick={() => setIsOpen(false)} />
+                  </span>
+                </div>
+                <div className={styles.body}>
+                  <textarea value={encodeString} readOnly={true} className={styles.encodeString} ref={encodeStringRef} />
+                </div>
+                <div className={styles.footer}>
+                  <div className={styles.content}>
+                    <input type="button" value="コピー" onClick={handleCopyEncodeString} className={styles.copyButton} />
                   </div>
                 </div>
               </div>
             </div>
-          ), window.document.body)
-        )}
-        <FaCopy onClick={this.handleClickOpen} className={styles.copyIcon} />
-      </div>
-    );
-  }
+          </div>
+        ), window.document.body)
+      )}
+      <FaCopy onClick={() => setIsOpen(true)} className={styles.copyIcon} />
+    </div>
+  );
 }
