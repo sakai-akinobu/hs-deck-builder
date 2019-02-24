287fa602bebec873798e8815977d79d908963c25
sakai-akinobu <sakai.akinobu@gmail.com>
2019-02-24 15:49:18 +0900

    Fix ESLint options.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   extends: [
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   rules: {
     "indent": "off",
