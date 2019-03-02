7e925bcadecad83f8e69485bb217b5d7c62c0757
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-02 17:17:44 +0900

    Fix styles.

--- client/src/components/deckBuild/CardCounter/index.tsx
+++ client/src/components/deckBuild/CardCounter/index.tsx
@@ -6,10 +6,18 @@ interface CardCounterProps {
   count: number;
 }
 
+const MAX_CARD_COUNT = 30;
+
 export default function CardCounter({ count }: CardCounterProps) {
   return (
     <div className={styles.container}>
-      <span className={styles.label}>({count}/30)</span>
+      <span
+        className={`${styles.label} ${
+          count === MAX_CARD_COUNT ? styles.isMax : ""
+        }`}
+      >
+        ({count}/{MAX_CARD_COUNT})
+      </span>
     </div>
   );
 }
