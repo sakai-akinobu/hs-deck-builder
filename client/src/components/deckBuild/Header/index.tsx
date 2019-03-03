5c0a584c8bd27e8edb758065f95ce1abb64eb4fe
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-03 15:32:41 +0900

    Add link to header.

--- client/src/components/deckBuild/Header/index.tsx
+++ client/src/components/deckBuild/Header/index.tsx
@@ -1,7 +1,17 @@
 import * as React from "react";
+import { FaGithub } from "react-icons/lib/fa";
 
 import styles from "./index.scss";
 
 export default function Header() {
-  return <h1 className={styles.header}>Hearthstone deck builder</h1>;
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.title}>Hearthstone deck builder</h1>
+      <nav className={styles.nav}>
+        <a href="https://github.com/sakai-akinobu/hs-deck-builder">
+          <FaGithub />
+        </a>
+      </nav>
+    </header>
+  );
 }
